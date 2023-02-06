2ab3e2fc4989f190c84e2897b8422d002267f985
koga yusuke <popweb782@gmail.com>
2023-02-06 23:17:19 +0900

    add Next.js Progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { Layout } from "@/components/@layouts/Layout";
 import "@/styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
 
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout {...pageProps}>
+      <NextNProgress color="#000" height={1} />
       <Component {...pageProps} />
     </Layout>
   );
