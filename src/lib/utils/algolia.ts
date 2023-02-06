fa39c96a2a0125b2ce2b8a7b742f73576c8d3b1c
koga yusuke <popweb782@gmail.com>
2023-02-06 22:01:18 +0900

    fix: error

--- src/lib/utils/algolia.ts
+++ src/lib/utils/algolia.ts
@@ -1,13 +1,13 @@
-import type { AlgoliaSearchObjectRequest } from '~/types/algolia';
-import type { NotionPost } from '~/types/notion';
+import type { AlgoliaSearchObjectRequest } from "~/types/algolia";
+import type { NotionPost } from "@/types/notion";
 
-import algoliasearch from 'algoliasearch';
+import algoliasearch from "algoliasearch";
 
 const client = algoliasearch(
   `${process.env.ALGOLIA_APPLICATION_ID}`,
   `${process.env.ALGOLIA_ADMIN_API_KEY}`
 );
-const index = client.initIndex('noblog');
+const index = client.initIndex("noblog");
 
 export const saveToAlgolia = async (post: NotionPost) => {
   try {
@@ -15,11 +15,11 @@ export const saveToAlgolia = async (post: NotionPost) => {
       .map((child) => {
         // @ts-expect-error ignore
         const content = child[child.type];
-        if (!('rich_text' in content && content.rich_text.length > 0)) return;
+        if (!("rich_text" in content && content.rich_text.length > 0)) return;
 
-        return content.rich_text.map((text: any) => text.plain_text).join('');
+        return content.rich_text.map((text: any) => text.plain_text).join("");
       })
-      .join('');
+      .join("");
 
     const records: AlgoliaSearchObjectRequest = {
       objectID: post.id,
