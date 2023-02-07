30bf563a9163b5e9d785fcd0b57252bc10f2429b
koga yusuke <popweb782@gmail.com>
2023-02-07 17:25:19 +0900

    fix: update title-2

--- src/components/@layouts/Layout.tsx
+++ src/components/@layouts/Layout.tsx
@@ -1,12 +1,28 @@
 import type { FC, ReactNode } from "react";
+import { clsx } from "@mantine/core";
+import { useRouter } from "next/router";
+import { SITE_TITLE } from "@/constants";
 
 type Props = {
   children: ReactNode;
 };
 
 export const Layout: FC<Props> = ({ children, ...pageProps }) => {
+  const router = useRouter();
+
   return (
     <div className="bg-slate-100">
+      <header className="py-1">
+        <div
+          className={clsx(
+            "mx-auto w-fit cursor-pointer py-4",
+            "hover:title-drop-shadow transition duration-1000 ease-in hover:text-white"
+          )}
+          onClick={() => router.push("/")}
+        >
+          <h1 className="font-baloo text-[42px] leading-none">{SITE_TITLE}</h1>
+        </div>
+      </header>
       <main className="relative z-10 mb-40 min-h-[calc(100vh-102px)] w-full bg-slate-100">
         <div className=" mx-auto max-w-[1280px]">{children}</div>
       </main>
